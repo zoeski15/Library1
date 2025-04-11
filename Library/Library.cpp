2f5f45b76651cbdc81f822de5358bd096faa614c
zoe cavazzoni <zoecvz15@gmail.com>
2025-04-12 02:15:38 +1000

    methods

--- Library/Library.cpp
+++ Library/Library.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 
@@ -6,39 +5,53 @@ using namespace std;
 
 int main() {
 
+class Book {
+private:
+	string title;
+	string author;
+	string isbn;
+	bool availability;
+	string dateAdd;
 
-	class Book {
-	private:
-		string title;
-		string author;
-		string isbn;
-		bool availability;
-		string dateAdd;
+public:
+	Book() : title(""), author(""), isbn(""), availability(true), dateAdd("") {}
 
-	public:
-		Book() : title(""), author(""), isbn(""), availability(true), dateAdd("") {}
+	void setBookDetails(string t, string a, string i, string d) {
+		title = t;
+		author = a;
+		isbn = i;
+		availability = true;
+		dateAdd = d;
+	}
 
-		void setBookDetails(string t, string a, string i, string d) {
+	void displayBookDetails() const {
+		cout << "Title: " << title << endl;
+		cout << "Author: " << author << endl;
+		cout << "ISBN: " << isbn << endl;
+		cout << "Availability: " << (availability ? "Available" : "Borrowed") << endl;
+		cout << "Date Added: " << dateAdd << endl;
+		cout << "------" << endl;
+	}
 
-			title = t;
-			author = a;
-			isbn = i;
-			availability = true;
-			dateAdd = d;
+	string getisbn() const {
+		return isbn;
+	}
+
+	bool isAvailable() const{
+		return availability;
+	}
+
+	bool hasBeenBorrowed() {
+		if(availability) {
+			availability = false;
+			return true;
 		}
+		return false;
+	   }
+	void returnBook() {
+		availability = true;
+	}
+};
 
-		void displayBookDetails() const {
-			cout << "Title" << title << endl;
-			cout << "Author" << author << endl;
-			cout << "isbn" << isbn << endl;
-			cout << "Availability" << (availability ? "Available" : "Borrowed") << endl;
-			cout << "Date Added" << dateAdd << endl;
-			cout << "------" << endl;
-
-		}
-	};
-
-	return 0;
+return 0;
 }
-
-
