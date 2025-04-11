b2767141531734023f0bcd076113535170dfd4f1
zoe cavazzoni <zoecvz15@gmail.com>
2025-04-12 02:05:43 +1000

    fixed start

--- Library/Library.cpp
+++ Library/Library.cpp
@@ -4,18 +4,21 @@
 
 using namespace std;
 
-   
+int main() {
+
+
 	class Book {
+	private:
 		string title;
-	string author;
-	string isbn;
-	bool availability;
-	string dateAdd;
+		string author;
+		string isbn;
+		bool availability;
+		string dateAdd;
 
-public:
-	Book() : title(""), author(""), isbn(""), availability(true), dateAdd("") {}
+	public:
+		Book() : title(""), author(""), isbn(""), availability(true), dateAdd("") {}
 
-	void setBookDetails(string t, string a, string i, string d) {
+		void setBookDetails(string t, string a, string i, string d) {
 
 			title = t;
 			author = a;
@@ -24,20 +27,18 @@ public:
 			dateAdd = d;
 		}
 
-	void displayBookDetails() const {
-		cout << "Title" << title << endl;
-		cout << "Author" << author << endl;
-		cout << "isbn" << isbn << endl;
-		cout << "Availability" << (availability ? "Available" : "Borrowed") << endl;
-		cout << "Date Added" << dateAdd << endl;
-		cout << "------" << endl;
+		void displayBookDetails() const {
+			cout << "Title" << title << endl;
+			cout << "Author" << author << endl;
+			cout << "isbn" << isbn << endl;
+			cout << "Availability" << (availability ? "Available" : "Borrowed") << endl;
+			cout << "Date Added" << dateAdd << endl;
+			cout << "------" << endl;
 
-	}
-	 
+		}
+	};
 
-
-
-
+	return 0;
 }
 
 
